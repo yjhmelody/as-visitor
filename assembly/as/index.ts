0afb7baaef567dd4ef9f19fd1af3c89b601d6d78
yjhmelody <465402634@qq.com>
2021-04-29 15:43:44 +0800

    add eslint

--- assembly/as/index.ts
+++ assembly/as/index.ts
@@ -2,4 +2,5 @@ export * from "./ast";
 export * from "./common";
 export * from "./tokenizer";
 export * from "./diagnostics";
+export * from "./diagnosticMessages.generated";
 export * from "./util";
